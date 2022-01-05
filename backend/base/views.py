ceb09371528170711733b619b394ca02e0a407e4
PlatoSys <platoniinjgia@gmail.com>
2022-01-05 23:24:11 +0400

    Serializers

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,5 @@
+from .models import Product
+from .serializers import ProductSerializer
 from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -11,13 +13,12 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
