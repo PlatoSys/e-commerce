b2c834c434b15ab4cd0c3b46eae1a1f5eef99264
PlatoSys <platoniinjgia@gmail.com>
2022-01-16 20:40:23 +0400

    Travis

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ import environ
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEST_DATABASE_PREFIX = ""
+TEST_DATABASE_PREFIX = "test_db"
 
 env = environ.Env()
 environ.Env.read_env()
