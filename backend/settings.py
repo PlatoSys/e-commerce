86ce9505e8524bb86067266aa0a37e86777188e2
PlatoSys <platoniinjgia@gmail.com>
2022-01-16 22:47:20 +0400

    Travis DB

--- backend/settings.py
+++ backend/settings.py
@@ -136,8 +136,6 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'travis_ci_test',
             'USER': 'postgres',
-            # 'HOST': 'localhost',
-            # 'PORT': '5432',
             }
     }
 }
