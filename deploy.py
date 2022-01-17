ee1778b176573ab4549a7a29bbdc90b7ff3a764a
PlatoSys <platoniinjgia@gmail.com>
2022-01-17 13:12:29 +0400

    Docker

--- deploy.py
+++ deploy.py
@@ -2,10 +2,16 @@ import os
 
 BASE_DIR = os.getcwd()
 
-os.chdir(f'{BASE_DIR}/frontend')
-os.system('npm run build')
-os.chdir(f'{BASE_DIR}')
-os.system('python manage.py collectstatic --noinput')
+react = input("React static?: ")
+react = False if react.lower() == 'no' else True
+if react:
+    os.chdir(f'{BASE_DIR}/frontend')
+    os.system('npm run build')
+django = input("Django static?: ")
+django = False if django.lower() == 'no' else True
+if django:
+    os.chdir(f'{BASE_DIR}')
+    os.system('python manage.py collectstatic --noinput')
 os.system('git add .')
 commit = input('Input Commit Message: ')
 os.system(f'git commit -m "{commit}"')
